820be90ebef9ac9266ac0dab1ee66a5b1f944604
Jiaxuan Wang <jiaxuan@umich.edu>
2021-09-24 15:27:49 -0500

    change ccmr umodel default to None

--- scripts/ccm_r.py
+++ scripts/ccm_r.py
@@ -72,7 +72,7 @@ def get_args():
                         default="gold_models/flip/concepts",
                         help="concept model path starting from root (ignore .pt)")
     parser.add_argument("--u_model_path", type=str,
-                        default="gold_models/crop/standard",
+                        default=None, # "gold_models/crop/standard",
                         help="unknown concept model path starting from root (ignore .pt), if None then train from scratch")
     parser.add_argument("--concept_path", type=str,
                         default="outputs/concepts/concepts_108.txt",
