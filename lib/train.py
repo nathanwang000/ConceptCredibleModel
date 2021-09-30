cab78080b712b2e1d41ac942d65b0f3effedd2ee
Jiaxuan Wang <jiaxuan@umich.edu>
2021-09-30 16:19:59 -0500

    updated train_step to not get s

--- lib/train.py
+++ lib/train.py
@@ -26,7 +26,6 @@ def train_step_standard(net, loader, opt, criterion, device='cpu', **kwargs):
     losses = []    
     for x, y in tqdm.tqdm(loader, desc="train step for 1 epoch"):
         x, y = x.to(device), y.to(device)
-        x, s = CUB_shortcut_transform(x, y, **kwargs)
         opt.zero_grad()
         o = net(x)
         l = criterion(o, y).mean()
@@ -46,7 +45,6 @@ def train_step_xyc(net, loader, opt, criterion, independent=False, device='cpu',
     
     for x, y, c in tqdm.tqdm(loader, desc="train step for 1 epoch xyc"):
         x, y, c = x.to(device), y.to(device), c.to(device).float()
-        x, s = CUB_shortcut_transform(x, y, **kwargs)
         opt.zero_grad()
 
         if type(net) == CBM:
